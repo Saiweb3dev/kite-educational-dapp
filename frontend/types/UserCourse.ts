93cec453bb81acce05360b712add8caa80476931
saik434455@gmail.com <saik434455@gmail.com>
2024-08-15 13:41:07 +0530

    added the quiz types

--- frontend/types/UserCourse.ts
+++ frontend/types/UserCourse.ts
@@ -9,9 +9,35 @@ export interface Quiz {
   answerIndex?: number; // Optional field to store the selected answer index
 }
 
+export interface QuizResultProps {
+  quizResult: {
+    correct_answers: number;
+    total_questions: number;
+  };
+  onClose: () => void; // Function to close the modal
+}
+
 export interface CourseDetails {
   name: string;
   description: string;
   imageUrl: string;
   quizzes: Quiz[];
+}
+
+interface Answer {
+  questionId: number;
+  selectedOption: string;
+}
+
+export interface CheckAnswersResponse {
+  address: string;
+  correct_answers: number;
+  total_questions: number;
+}
+
+export interface CheckAnswerButtonProps {
+  courseName: string;
+  answers: Answer[];
+  onResultReceived: (result: CheckAnswersResponse) => void;
+  address: string | undefined;
 }
