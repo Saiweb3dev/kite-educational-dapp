f8d0cae219d9e1b63caf15a07eeaf541fbcc1815
saik434455@gmail.com <saik434455@gmail.com>
2024-08-16 21:22:03 +0530

    feat: pkg & config updated

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,37 @@
-require("@nomicfoundation/hardhat-toolbox");
+// Importing necessary packages
+const dotenv = require('dotenv');
+ require("hardhat-deploy");
+require("@nomicfoundation/hardhat-toolbox")
+// Configuring dotenv
+dotenv.config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.24",
+// Defining the Hardhat configuration object
+const config = {
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    // Other networks...
+  },  
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: true,
+    src: "./contracts",
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v6"
+  }
 };
+
+// Exporting the configuration
+module.exports = config;
