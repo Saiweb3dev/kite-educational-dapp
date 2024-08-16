dd529a9e49c0558c1f03b8c40cf3fd49065367d2
saik434455@gmail.com <saik434455@gmail.com>
2024-08-16 15:07:33 +0530

    added props

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,8 +1,8 @@
-const Loader = () => {
+const Loader = ({ message = "Please wait a sec..." }) => {
   return (
     <div className="flex justify-center items-center space-x-4 h-screen bg-white">
       <span className="loading loading-spinner text-info"></span>
-      <p className="text-xl text-blue-700">Please wait a sec...</p>
+      <p className="text-xl text-blue-700">{message}</p>
     </div>
   );
 };
