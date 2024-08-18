a9504022f10f0e84710a6edbb1ecadc55947bf96
saik434455@gmail.com <saik434455@gmail.com>
2024-08-18 15:25:14 +0530

    installed new pkg and some contract file config

---
+++ frontend/constants/index.ts
@@ -0,0 +1,11 @@
+// constants/index.ts
+
+// Import the JSON files using ES module syntax
+import contractAddress from './KITE_EEDU_NFT_ADDRESS.json';
+import abi from './KITE_EEDU_NFT_ABI.json';
+
+// Export the imported variables
+export {
+ abi,
+ contractAddress,
+};
